34fd7a1cae9dac6e65cee238e17a98b3203f1825
gloirekiba <gloirekiba21@gmail.com>
2022-10-13 23:48:02 +0200

    ✨ add server

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import morgan from "morgan";
+import express, { Application } from "express";
+
+import indexRouter from "./routes/index.route";
+
+const app: Application = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
