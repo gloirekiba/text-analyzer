3476e9a16bbbe5873f07e48c2dd657bca08f72cc
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 00:45:34 +0200

    ✨ add new features

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,14 +1,28 @@
-import express from "express";
+import express, { Request, Response, Router } from "express";
 
-const router = express.Router();
+import Text from "../helpers/Text";
 
-router.get("/", (req, res) => {
+const router: Router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
   res.render("index", { title: "Express" });
 });
 
-router.post("/", (req, res) => {
-  const text = req.body.text;
-  res.send(text);
+router.post("/", (req: Request, res: Response) => {
+  const text = req.body.text as string;
+
+  const stats = [
+    { name: "Total Words", value: Text.totalWordsClean(text) },
+    { name: "Unique Words", value: Text.uniqueWordsClean(text) },
+    { name: "Average Word Length", value: Text.averageWordLength(text) },
+    { name: "Total Characters", value: text.length },
+    {
+      name: "Total Characters (no spaces)",
+      value: text.replace(/\s/g, "").length,
+    },
+  ];
+
+  res.render("stats", { title: "Express", stats });
 });
 
 export default router;
