f41757e8c172c24e09025dd3ce7cbaaf945fb16e
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 10:33:06 +0200

    ♻️ refactor code

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,25 +1,11 @@
 import express, { Request, Response, Router } from "express";
 
 import { APP_NAME } from "../config/default";
-import Text from "../helpers/Text";
-import { getTextStats } from "../helpers/helpers";
 
 const router: Router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.render("index", { title: APP_NAME });
-});
-
-router.get("/stats", (req: Request, res: Response) => {
-  res.render("stats", { title: APP_NAME });
-});
-
-router.post("/stats", (req: Request, res: Response) => {
-  const text = req.body.text as string;
-  const stats = getTextStats(text);
-  const words = Text.wordRepetition(text);
-
-  res.render("stats", { title: APP_NAME, stats, words });
 });
 
 export default router;
