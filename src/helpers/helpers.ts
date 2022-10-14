852aaba0b1689e3ae5a249db333871bd7201331f
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 02:19:42 +0200

    ✨ add helpers.ts

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,15 @@
+import Text from "./Text";
+
+export function getTextStats(text: string): { name: string; value: number }[] {
+  return [
+    { name: "Total Words", value: Text.totalWordsClean(text) },
+    { name: "Unique Words", value: Text.uniqueWordsClean(text) },
+    { name: "Average Word Length", value: Text.averageWordLength(text) },
+    { name: "Total Characters", value: text.length },
+    {
+      name: "Total Characters (no spaces)",
+      value: text.replace(/\s/g, "").length,
+    },
+    { name: "Total Sentences", value: Text.totalSentences(text) },
+  ];
+}
