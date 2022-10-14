bf2e0e8da3aa898054e06b36b1e74cca12dfa7e9
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 02:20:13 +0200

    💡 update source code

--- src/public/js/stats.js
+++ src/public/js/stats.js
@@ -1,21 +1,18 @@
-const filter = document.querySelector("#filter");
+const filter = document.getElementById("filter");
+const listRepetition = document.getElementById("list-repetition");
 
-console.log(filter);
+filter.addEventListener("keyup", (e) => {
+  const text = e.target.value.toLowerCase();
+  const rows = listRepetition.querySelectorAll("tr");
 
-// const listRepetition = document.getElementById("list-repetition");
-
-// filter.addEventListener("keyup", (e) => {
-//   const text = e.target.value.toLowerCase();
-//   const rows = listRepetition.querySelectorAll("tr");
-
-//   rows.forEach((row) => {
-//     const name = row.querySelector("td").textContent;
-//     if (name.toLowerCase().indexOf(text) !== -1) {
-//       row.style.display = "table-row";
-//     } else {
-//       row.style.display = "none";
-//     }
-//   });
-// });
+  rows.forEach((row) => {
+    const name = row.querySelector("td").textContent;
+    if (name.toLowerCase().indexOf(text) !== -1) {
+      row.style.display = "table-row";
+    } else {
+      row.style.display = "none";
+    }
+  });
+});
 
 console.log("hello");
