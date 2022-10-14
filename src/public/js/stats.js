a078cc9fe65e1eed529dd82ae386db0fd271fb9a
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 02:06:55 +0200

    ✨ add filter feature

---
+++ src/public/js/stats.js
@@ -0,0 +1,21 @@
+const filter = document.querySelector("#filter");
+
+console.log(filter);
+
+// const listRepetition = document.getElementById("list-repetition");
+
+// filter.addEventListener("keyup", (e) => {
+//   const text = e.target.value.toLowerCase();
+//   const rows = listRepetition.querySelectorAll("tr");
+
+//   rows.forEach((row) => {
+//     const name = row.querySelector("td").textContent;
+//     if (name.toLowerCase().indexOf(text) !== -1) {
+//       row.style.display = "table-row";
+//     } else {
+//       row.style.display = "none";
+//     }
+//   });
+// });
+
+console.log("hello");
