337b7fa39f63a01fc24d15ad1605aaab40043b88
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 10:32:55 +0200

    🥅 add route error handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import path from "path";
 import morgan from "morgan";
-import express, { Application } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 
 import indexRouter from "./routes/index.route";
+import statsRouter from "./routes/stats.route";
 
 const app: Application = express();
 
@@ -13,6 +14,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
+app.use("/stats", statsRouter);
+
+// Error handling
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err = new Error("Pag Not Found") as any;
+  res.status(404);
+  next(err);
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error", { title: "Error", message: err.message });
+});
 
 const PORT = process.env.PORT || 3000;
 
