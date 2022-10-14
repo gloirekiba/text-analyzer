cd6267e0f622b0f80e0d76fb15cb208200819b17
gloirekiba <gloirekiba21@gmail.com>
2022-10-14 03:07:48 +0200

    ✨ add new endpoint

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response, Router } from "express";
 
+import { APP_NAME } from "../config/default";
 import Text from "../helpers/Text";
 import { getTextStats } from "../helpers/helpers";
 
 const router: Router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: APP_NAME });
+});
+
+router.get("/stats", (req: Request, res: Response) => {
+  res.render("stats", { title: APP_NAME });
 });
 
 router.post("/stats", (req: Request, res: Response) => {
@@ -14,7 +19,7 @@ router.post("/stats", (req: Request, res: Response) => {
   const stats = getTextStats(text);
   const words = Text.wordRepetition(text);
 
-  res.render("stats", { title: "Express", stats, words });
+  res.render("stats", { title: APP_NAME, stats, words });
 });
 
 export default router;
